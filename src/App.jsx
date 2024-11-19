d1160518ac979e9562b41afbf82c0d04e7700fbb
cristiandevdlc <Fernanda.brown0192@gmail.com>
2024-11-18 20:41:14 -0600

    add

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,6 @@ import Hero from './components/Hero';
 import Experience from './components/Experience';
 import Skills from './components/Skills';
 import Projects from './components/Projects';
-import Contact from './components/Contact';
 import AboutMe from './components/AboutMe';
 
 function App() {
